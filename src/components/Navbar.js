4761aca69f4f867d8a68c2181ec40d0b2a83d3a1
Thanh Le <thanhlevan0203@gmail.com>
2022-07-17 18:39:13 -0700

    Created Navbar component, Home Page

---
+++ src/components/Navbar.js
@@ -0,0 +1,103 @@
+
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+const Container = styled.div`
+  height: 60px;
+  background-color: none;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+
+`
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  fullwidth;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+
+const Input = styled.input`
+  border: none;
+`
+
+const Logo = styled.h1`
+  font-weight: bold;
+`
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`
+
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{color: "gray", fontSize: 16}} />
+    
+          </SearchContainer>
+        </Left>
+    
+        <Center>
+          <Logo>
+            Tikita
+          </Logo>
+        </Center>
+
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+          <Badge badgeContent={4} color="primary">
+            <ShoppingCartOutlinedIcon color="action" />
+          </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+
+  )
+}
+
+export default Navbar
