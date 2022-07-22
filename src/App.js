50a72b9530c28108cf764958a03fe3be383dfec5
Thanh Le <thanhlevan0203@gmail.com>
2022-07-22 01:22:29 -0700

    Created Shopping Cart Page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react'
 import Home from './Pages/Home'
 import Product from './Pages/Product'
 import Register from './Pages/Register'
+import Login from './Pages/Login'
+import Cart from './Pages/Cart'
 
 const App = () => {
   return (
-    <Register />
+    <Cart />
   )
 }
 
---
+++ src/Pages/Cart.js
@@ -0,0 +1,221 @@
+
+import React from 'react'
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Container = styled.div``
+const Wrapper = styled.div`
+  padding: 20px;
+`
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${props => 
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`
+const TopTexts = styled.div``
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const Info = styled.div`
+  flex: 3;
+`
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+const Image = styled.img`
+  width: 200px;
+  
+`
+const ProductName = styled.span``
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`
+const ProductId = styled.span``
+const ProductColor = styled.span`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+`
+const ProductSize = styled.span``
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${props => props.type === "total" && "500"};
+  font-size: ${props => props.type === "total" && "24px"};
+`
+const SummaryItemText = styled.span``
+const SummaryItemPrice = styled.span``
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-size: 600;
+`
+const SummaryItemTax = styled.span``
+
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type='filled'>CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6509/6509650_sd.jpg" />
+                <Details>
+                  <ProductName><b>Product:</b> Macbook Air</ProductName>
+                  <ProductId><b>ID:</b> 9310101010</ProductId>
+                  <ProductColor color='black' />
+                  <ProductSize><b>Size:</b> 13"</ProductSize>
+                </Details>
+              </ProductDetail>
+
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <AddIcon />
+                  <ProductAmount>2</ProductAmount>
+                  <RemoveIcon />
+                </ProductAmountContainer>
+                <ProductPrice>$999.99</ProductPrice>
+              </PriceDetail>
+            </Product>
+
+            <Hr />
+
+            <Product>
+              <ProductDetail>
+                <Image src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6494/6494230cv12d.jpg" />
+                <Details>
+                  <ProductName><b>Product:</b> Macbook Air</ProductName>
+                  <ProductId><b>ID:</b> 9310101010</ProductId>
+                  <ProductColor color='Gray' />
+                  <ProductSize><b>Size:</b> 14.6"</ProductSize>
+                </Details>
+              </ProductDetail>
+
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <AddIcon />
+                  <ProductAmount>1</ProductAmount>
+                  <RemoveIcon />
+                </ProductAmountContainer>
+                <ProductPrice>$1,199.99</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$3999.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemTax>Tax</SummaryItemTax>
+              <SummaryItemPrice>$399.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimate Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 19.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -19.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$4199.99</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  )
+}
+
+export default Cart
