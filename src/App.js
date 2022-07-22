7e20500cf0a07016fe3ca46e99fac1819461a4f3
Thanh Le <thanhlevan0203@gmail.com>
2022-07-21 23:03:03 -0700

    Created an Account Users

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css'
 import React from 'react'
 import Home from './Pages/Home'
 import Product from './Pages/Product'
+import Register from './Pages/Register'
 
 const App = () => {
   return (
-    <Product />
+    <Register />
   )
 }
 
