c0a7d65a370caac181a6f19d7f52b63d91c50c7a
Thanh Le <thanhlevan0203@gmail.com>
2022-07-21 23:32:21 -0700

    Completed Register Page

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -2,13 +2,51 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
-const Wrapper = styled.div``
-const Title = styled.h1``
-const Form = styled.form``
-const Input = styled.input``
-const Agreement = styled.div``
-const Button = styled.button``
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255,255,255,0.5), 
+    rgba(255,255,255,0.5)
+  ),
+   url("https://img.wallpapersafari.com/desktop/1600/900/64/14/wrdt3K.jpg")
+   center;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+`
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+`
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`
 
 const Register = () => {
   return (
@@ -16,7 +54,7 @@ const Register = () => {
       <Wrapper>
         <Title> CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder="name" />
+          <Input placeholder="first name" />
           <Input placeholder="last name" />
           <Input placeholder="username" />
           <Input placeholder="email" />
